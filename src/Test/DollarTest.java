308364558c22e98389e660df9cc69e8951b9dfd3
Julien <julien.candela@centrale-marseille.fr>
2015-01-09 14:48:17 +0100

    Chapter 2 : times method return a Dollar

--- src/Test/DollarTest.java
+++ src/Test/DollarTest.java
@@ -7,8 +7,9 @@ public class DollarTest {
     public void testMultiplication() {
 
         Dollar five = new Dollar(5);
-        five.times(2);
-        Assert.assertEquals(10, five.amount);
-        
+        Dollar product= five.times(2);
+        Assert.assertEquals(10, product.amount);
+        product = five.times(3);
+        Assert.assertEquals(15, product.amount);
     }
 }
