071be5f140f5f5a26120958df0ae5340890eb612
Julien <julien.candela@centrale-marseille.fr>
2015-01-17 19:43:54 +0100

    Chapter 13 : reducing Expressions

---
+++ src/Main/Sum.java
@@ -0,0 +1,19 @@
+/**
+ * Created by marie-helene on 17/01/15.
+ */
+public class Sum implements Expression {
+
+    Money augend;
+    Money addend;
+
+    Sum(Money augend, Money addend){
+        this.augend=augend;
+        this.addend=addend;
+    }
+
+
+    public Money reduce(String to) {
+        int amount = augend.amount + addend.amount;
+        return new Money(amount, to);
+    }
+}
--- src/Test/MoneyTest.java
+++ src/Test/MoneyTest.java
@@ -42,5 +42,30 @@ public class MoneyTest {
         Assert.assertEquals(Money.dollar(10), reduced);
     }
 
+    @Test
+    public void testPlusReturnsSum(){
+        Money five = Money.dollar(5);
+        Expression result = five.plus(five);
+        Sum sum = (Sum) result;
+        Assert.assertEquals(five, sum.augend);
+        Assert.assertEquals(five, sum.addend);
+
+    }
+
+    @Test
+    public void testReduceSum(){
+        Expression sum= new Sum(Money.dollar(3), Money.dollar(4));
+        Bank bank= new Bank();
+        Money result= bank.reduce(sum, "USD");
+        Assert.assertEquals(Money.dollar(7), result);
+    }
+
+    @Test
+    public void testReduceMoney(){
+        Bank bank= new Bank();
+        Money result= bank.reduce(Money.dollar(1), "USD");
+        Assert.assertEquals(Money.dollar(1), result);
+    }
+
 
 }
